3ae56610844f794a033f5821cffd06d8f0e4c48b
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-02 16:22:25 +0800

    [add] 为工具页副栏添加动画

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,41 +1,46 @@
 <template>
-  <!--  <div class="fixed top-0 h-16 w-full bg-white backdrop-blur-sm shadow hover:shadow-md transition-shadow flex flex-col items-center">-->
-  <!--    <div class="w-full h-full px-4 container flex justify-between items-center">-->
-  <div
-    class="fixed top-0 w-full bg-white backdrop-blur-sm shadow hover:shadow-md transition-shadow flex flex-col items-center"
-    :class="{'h-24': isToolPage(), 'h-16': !isToolPage()}"
-  >
-    <div class="w-full h-16 px-4 container flex justify-between items-center">
-      <div>
-        <nuxt-link :to="localePath('/')" class="text-lg font-bold">CTFever</nuxt-link>
-      </div>
-      <div>
-        <a-dropdown :trigger="['click']">
-          <a class="ant-dropdown-link" @click="e => e.preventDefault()">
-            {{ $t('topbar.langTip') }}
-          </a>
-          <a-menu slot="overlay">
-            <a-menu-item v-for="(locale, k) in availableLocales" :key="k">
-              <nuxt-link
-                class="px-2 py-px"
-                :key="locale.code"
-                :to="switchLocalePath(locale.code)">{{ locale.name }}
-              </nuxt-link>
-            </a-menu-item>
-          </a-menu>
-        </a-dropdown>
-        <TinyButtonLink class="ml-1" :to="localePath('/login')">{{ $t('topbar.login') }}</TinyButtonLink>
-        <TinyButtonLink class="ml-1" :to="localePath('/signup')" accent>{{ $t('topbar.signup') }}</TinyButtonLink>
+  <div>
+    <div
+      class="fixed z-10 top-0 w-full bg-white backdrop-blur-sm transition-shadow flex flex-col items-center h-16"
+      :class="{'shadow hover:shadow-md': !isToolPage()}"
+    >
+      <div class="w-full h-16 px-4 container flex justify-between items-center">
+        <div>
+          <nuxt-link :to="localePath('/')" class="text-lg font-bold">CTFever</nuxt-link>
+        </div>
+        <div>
+          <a-dropdown :trigger="['click']">
+            <a class="ant-dropdown-link" @click="e => e.preventDefault()">
+              {{ $t('topbar.langTip') }}
+            </a>
+            <a-menu slot="overlay">
+              <a-menu-item v-for="(locale, k) in availableLocales" :key="k">
+                <nuxt-link
+                  class="px-2 py-px"
+                  :key="locale.code"
+                  :to="switchLocalePath(locale.code)">{{ locale.name }}
+                </nuxt-link>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+          <TinyButtonLink class="ml-1" :to="localePath('/login')">{{ $t('topbar.login') }}</TinyButtonLink>
+          <TinyButtonLink class="ml-1" :to="localePath('/signup')" accent>{{ $t('topbar.signup') }}</TinyButtonLink>
+        </div>
       </div>
     </div>
     <div
-      v-if="isToolPage()"
-      class="w-full h-8 px-4 container flex justify-between items-center border-t border-t-gray-200">
-      <div>
-        <nuxt-link :to="localePath('/')">← 返回</nuxt-link>
-      </div>
-      <div>
-        <button>⭐</button>
+      class="fixed z-0 top-16 w-full h-8 bg-white backdrop-blur-sm -translate-y-8 transition flex flex-col items-center h-16"
+      :class="{'shadow hover:shadow-md opacity-100 translate-y-0': isToolPage()}"
+    >
+      <div
+        class="w-full h-full px-4 container flex justify-between items-center border-t border-t-gray-150"
+      >
+        <div>
+          <nuxt-link :to="localePath('/')">← 返回</nuxt-link>
+        </div>
+        <div>
+          <button>⭐</button>
+        </div>
       </div>
     </div>
   </div>
