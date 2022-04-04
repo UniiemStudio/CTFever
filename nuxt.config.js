0a530b5de3835850ad032f953514ecffc6bd5378
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-05 03:36:27 +0800

    [FIX] Update viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     title: 'CTFever Toolkit by uniiem',
     meta: [
       {charset: 'utf-8'},
-      {title: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {title: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0;'},
       {hid: 'description', title: 'description', content: ''},
       {title: 'format-detection', content: 'telephone=no'}
     ],
