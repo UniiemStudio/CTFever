7e992390a5560d931d7b3eb77093c18fa9483059
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-16 18:36:30 +0800

    [FEAT] JSFuck 编码工具允许使用 eval 包裹

--- pages/tools/jsfuck.vue
+++ pages/tools/jsfuck.vue
@@ -8,6 +8,7 @@
       <InteractiveBlock class="flex items-center justify-between">
         <div class="space-x-1">
           <PrimaryButton type="button" @click="encode">编码 ↓</PrimaryButton>
+          <a-checkbox v-model:checked="wrapWithEval">用 eval 函数包裹</a-checkbox>
           <!--          <PrimaryButton type="button" @click="decode">解码 ↑</PrimaryButton>-->
         </div>
         <PrimaryButton type="button" danger @click="input_code = ''; output = '';">清空</PrimaryButton>
@@ -25,9 +26,9 @@ import PrimaryContainer from "~/components/tool/PrimaryContainer";
 import InteractiveBlock from "~/components/tool/InteractiveBlock";
 import PrimaryArea from "~/components/form/PrimaryTextArea";
 import PrimaryButton from "~/components/form/PrimaryButton";
+import PrimaryPreBlock from "~/components/form/PrimaryPreBlock";
 
 import Jsfuck from 'jsfuck';
-import PrimaryPreBlock from "~/components/form/PrimaryPreBlock";
 
 export default {
   name: "jsfuck",
@@ -36,11 +37,12 @@ export default {
     return {
       input_code: "",
       output: "",
+      wrapWithEval: true,
     };
   },
   methods: {
     encode() {
-      this.output = Jsfuck.JSFuck.encode(this.input_code);
+      this.output = Jsfuck.JSFuck.encode(this.input_code, this.wrapWithEval);
     },
   },
 }
