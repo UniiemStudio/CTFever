e60f08768ce67a4500e63a2f853799011fba488f
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-17 17:36:38 +0800

    Premium introduction

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -30,8 +30,7 @@
               </a-menu-item>
             </a-menu>
           </a-dropdown>
-          <TinyButtonLink :to="localePath('/login')">{{ $t('topbar.login') }}</TinyButtonLink>
-          <TinyButtonLink :to="localePath('/signup')" accent>{{ $t('topbar.signup') }}</TinyButtonLink>
+          <TinyButtonLink :to="localePath('/premium-active')" accent>Premium</TinyButtonLink>
         </div>
       </div>
     </div>
