55bd725f1d4ac11c87a244e62266352752bce8a5
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-19 10:45:38 +0800

    修改 prose code 样式，完善几处 intro

--- components/tool/PrimaryIntroduction.vue
+++ components/tool/PrimaryIntroduction.vue
@@ -2,7 +2,7 @@
   <div class="lg:m-8 lg:mt-6 p-4 bg-white rounded-lg border border-gray-200 bg-gray-50">
     <p class="font-bold text-lg mb-1.5" v-if="title">{{ title }}</p>
     <div class="mb-4" v-if="document">
-      <nuxt-content class="prose prose-sm max-w-none" :document="document"/>
+      <nuxt-content class="prose prose-sm max-w-none reset-code" :document="document"/>
     </div>
     <div class="flex flex-col" v-if="references">
       <p class="font-bold text-lg mb-1">References</p>
@@ -36,6 +36,8 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+.reset-code {
+  @apply prose-code:before:content-none prose-code:after:content-none prose-code:rounded prose-code:bg-gray-300 prose-code:py-0.5 prose-code:px-1 prose-code:mx-0.5;
+}
 </style>
