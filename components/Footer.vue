a5df75b14c69db2bdf318f39b65792756ef16f8d
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-30 14:35:05 +0800

    [FIX] 合并猪圈密码工具，修改猪圈密码样式

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
   <footer class="mx-auto pattern-diagonal-stripes-sm bg-gray-50 text-gray-100 lg:rounded
                 dark:bg-slate-900 dark:text-slate-800">
     <div class="w-full h-full py-4 flex flex-col text-sm justify-center items-center text-gray-500">
-      <p class="mt-2 text-gray-300">CTFever for everyone</p>
+      <p class="mt-2 dark:text-gray-300">CTFever for everyone</p>
       <p class="text-xs text-gray-400 mt-1">
         Made with <span><ion-icon class="animate-bounce animate-pulse text-red-400" name="heart"></ion-icon></span> by
         Uniiem, in memory of BoxMoe
--- pages/tools/pigpen.vue
+++ pages/tools/pigpen.vue
@@ -1,30 +1,42 @@
 <template>
-  <InteractiveBlock>
-    <table class="t-table pig-font pig-font-size" title="猪圈密码">
-      <tbody>
-      <tr v-for="(rowList,key) in valueList" :key="key">
-        <td v-for="(rowValue, i) in rowList" :key="i" @click="add(rowValue)">{{ rowValue }}</td>
-      </tr>
-      </tbody>
-    </table>
-    <InteractiveBlock>
-      <PrimaryInput id="output" label="密文" v-model="result" class="pig-font" disable/>
-      <PrimaryInput id="output" label="明文" v-model="result" disable/>
-    </InteractiveBlock>
-    <InteractiveBlock>
-      <PrimaryButton type="button" @click="clear()">清空</PrimaryButton>
-    </InteractiveBlock>
-  </InteractiveBlock>
+  <PrimaryContainer>
+    <InteractiveDoubleColumns>
+      <template v-slot:left>
+        <InteractiveBlock>
+          <div class="flex flex-col dark:text-slate-300">
+            <div v-for="(rowList,key) in valueList" :key="key"
+                 class="font-['pigpen-regular'] text-4xl w-full flex justify-around">
+              <span v-for="(rowValue, i) in rowList" :key="i" @click="add(rowValue)"
+                    class="hover:text-cyan-500 cursor-pointer">{{ rowValue }}</span>
+            </div>
+          </div>
+        </InteractiveBlock>
+      </template>
+      <template v-slot:right>
+        <InteractiveBlock>
+          <PrimaryInput id="output" label="密文" v-model="result" class="pig-font" disable/>
+        </InteractiveBlock>
+        <InteractiveBlock>
+          <PrimaryInput id="output" label="明文" v-model="result" disable/>
+        </InteractiveBlock>
+        <InteractiveBlock>
+          <PrimaryButton type="button" @click="clear">清空</PrimaryButton>
+        </InteractiveBlock>
+      </template>
+    </InteractiveDoubleColumns>
+  </PrimaryContainer>
 </template>
 
 <script>
 import InteractiveBlock from "~/components/tool/InteractiveBlock";
 import PrimaryInput from "~/components/form/PrimaryInput";
 import PrimaryButton from "~/components/form/PrimaryButton";
+import PrimaryContainer from "~/components/tool/PrimaryContainer";
+import InteractiveDoubleColumns from "~/components/tool/InteractiveDoubleColumns";
 
 export default {
   name: "pigpen",
-  components: {PrimaryButton, PrimaryInput, InteractiveBlock},
+  components: {InteractiveDoubleColumns, PrimaryContainer, PrimaryButton, PrimaryInput, InteractiveBlock},
   head() {
     return {
       title: this.$t("tool.pigpen.title") + " - " + this.$t("app.name")
@@ -34,17 +46,15 @@ export default {
     return {
       result: "",
       valueList: [
-        ['a','b','c','d','e','f','g','h','i'],
-        ['j','k','l','m','n','o','p','q','r'],
-        ['s','t','u','v','w','x','y','z',':'],
-        ['<','>','?','@',';','=','[','\\',']'],
-        ['^','_',`\``,'{','}','|','~','%','+'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+        ['j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'],
+        ['s', 't', 'u', 'v', 'w', 'x', 'y', 'z', ':'],
+        ['<', '>', '?', '@', ';', '=', '[', '\\', ']'],
+        ['^', '_', `\``, '{', '}', '|', '~', '%', '+'],
       ],
     };
   },
-  watch: {
-
-  },
+  watch: {},
   methods: {
     add(value) {
       this.result += value
@@ -61,21 +71,24 @@ export default {
   font-family: "pigpen-regular";
   src: url("../static/pigpen/pigpen-regular.woff2") format("woff2"),
   url("../static/pigpen/pigpen-regular.woff") format("woff");
-  font-weight:normal;
+  font-weight: normal;
   font-style: normal;
 }
 
-.t-table tbody tr:hover{
+.t-table tbody tr:hover {
   background-color: transparent;
 }
-td:hover{
+
+td:hover {
   color: #009688;
-  cursor:pointer;
+  cursor: pointer;
 }
-.pig-font{
-  font-family: "pigpen-regular",serif;
+
+.pig-font {
+  font-family: "pigpen-regular", serif;
 }
+
 .pig-font-size {
-  font-size:60px;
+  font-size: 60px;
 }
 </style>
