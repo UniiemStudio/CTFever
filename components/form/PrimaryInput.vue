fe7e19308957d052f336073abcb4c7a45cc1dff9
星野鈴美 <boxlab@foxmail.com>
2022-05-11 10:19:25 +0800

    [FEAT] Input 组件添加清空按钮，修复副顶栏被内容遮挡的问题 (#45)

--- components/form/PrimaryInput.vue
+++ components/form/PrimaryInput.vue
@@ -3,13 +3,19 @@
     <label v-if="label" class="block text-gray-700 dark:text-slate-300 text-sm font-bold mb-2" :for="id">
       {{ label }}
     </label>
-    <input
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
-             focus:ring transition duration-300
+    <div class="relative">
+      <input
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+             focus:ring transition duration-300 input-transition
              dark:bg-slate-800 dark:text-slate-300 dark:border-slate-600"
-      :class="{'text-gray-300 dark:text-slate-500 bg-gray-50': disable}"
-      :id="id" :type="type" :placeholder="placeholder" :value="value" @input="onInput" :disabled="disable"
-      :autocomplete="autocomplete"/>
+        :class="{'text-gray-300 dark:text-slate-500 bg-gray-50': disable}"
+        :id="id" :type="type" :placeholder="placeholder" :value="value" @input="onInput" :disabled="disable"
+        :autocomplete="autocomplete"/>
+      <button @mousedown="clean"
+              class="absolute right-2 text-slate-500 opacity-0 pointer-events-none translate-x-4 transition clean-btn">
+        <ion-icon class="text-xl" name="close-circle"></ion-icon>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -50,11 +56,24 @@ export default {
   methods: {
     onInput(event) {
       this.$emit('input', event.target.value);
+    },
+    clean() {
+      this.$emit('input', '');
     }
   },
 }
 </script>
 
 <style scoped>
+.clean-btn {
+  top: calc(50% - 0.625rem);
+}
 
+.input-transition:focus + .clean-btn {
+  @apply translate-x-0 opacity-50 pointer-events-auto;
+}
+
+.input-transition:focus + .clean-btn:hover {
+  @apply opacity-75;
+}
 </style>
