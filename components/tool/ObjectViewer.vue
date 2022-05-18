1d418d6e06c82a9381e05134cb76336196a578d7
HoshinoSuzumi <boxlab@foxmail.com>
2022-05-18 19:18:34 +0800

    [FIX] fix ObjectViewer text color

--- components/tool/ObjectViewer.vue
+++ components/tool/ObjectViewer.vue
@@ -4,8 +4,8 @@
       class="my-2 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
              border border-gray-200 dark:border-slate-500 rounded-lg p-4">
       <div v-for="(v, k) in data" class="mb-2">
-        <h1 class="text-base font-normal text-slate-300">{{ k }}</h1>
-        <p class="text-sm font-thin text-slate-500">{{ v }}</p>
+        <h1 class="text-base font-normal dark:text-slate-300">{{ k }}</h1>
+        <p class="text-sm font-thin dark:text-slate-500">{{ v }}</p>
       </div>
     </div>
   </div>
