bf35addafc6e47fb58bcc996f6f71a38aa133743
HoshinoSuzumi <boxlab@foxmail.com>
2022-06-08 15:47:38 +0800

    [FEAT] 端口扫描工具 summary

--- components/tool/ObjectViewer.vue
+++ components/tool/ObjectViewer.vue
@@ -5,8 +5,8 @@
              border border-gray-200 dark:border-slate-500 rounded-lg p-2">
       <div v-for="(v, k) in data" @click="copy(v)"
            class="cursor-pointer transition p-2 hover:bg-gray-100 rounded-lg active:shadow-inner">
-        <h1 class="text-base font-normal dark:text-slate-300">{{ k }}</h1>
-        <p class="text-sm font-thin dark:text-slate-500">{{ v }}</p>
+        <h1 class="text-base font-normal dark:text-slate-300 overflow-x-hidden">{{ k }}</h1>
+        <p class="text-sm font-thin dark:text-slate-500 overflow-x-hidden">{{ v }}</p>
       </div>
     </div>
     <div class="mt-1 text-xs italic text-gray-300 select-none flex flex-row justify-between">
@@ -33,6 +33,11 @@ export default {
     tip: {
       type: String,
       required: false,
+    },
+    noSort: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
   computed: {
@@ -40,10 +45,17 @@ export default {
       let result = '';
       if (this.object) {
         let object = this.object;
-        object = Object.keys(object).sort().reduce((r, k) => {
-          r[k] = object[k];
-          return r;
-        }, {});
+        if (this.noSort) {
+          object = Object.keys(object).reduce((r, k) => {
+            r[k] = object[k];
+            return r;
+          }, {});
+        } else {
+          object = Object.keys(object).sort().reduce((r, k) => {
+            r[k] = object[k];
+            return r;
+          }, {});
+        }
 
         result = {};
         if (this.map) {
