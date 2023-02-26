a236b22bde87e94af84e03f27120cdf92872babd
Restyled.io <commits@restyled.io>
2023-02-26 17:07:57 +0000

    Restyled by prettier

--- plugins/g-tag.js
+++ plugins/g-tag.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
-import VueGtag from 'vue-gtag'
+import Vue from "vue";
+import VueGtag from "vue-gtag";
 
-Vue.use(VueGtag, {config : {id : 'G-ZD59S22S8Y'}});
+Vue.use(VueGtag, { config: { id: "G-ZD59S22S8Y" } });
