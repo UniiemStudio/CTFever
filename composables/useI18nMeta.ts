8921713fca487ff30cd0a6ea11553eee4af4c1c0
HoshinoSuzumi <master@uniiem.com>
2023-10-09 11:34:19 +0800

    chore: wip

--- composables/useI18nMeta.ts
+++ composables/useI18nMeta.ts
@@ -1,17 +1,17 @@
 export const useI18nMeta = () => {
   const nuxtApp = useNuxtApp()
-  const i18n = computed(() => nuxtApp.$i18n)
+  const i18n = computed(() => nuxtApp?.$i18n?.t)
   const toolkit = {
-    label: (key: string) => i18n.value.t(`app.toolkit.${key}.label`),
-    descriptipn: (key: string) => i18n.value.t(`app.toolkit.${key}.description`)
+    label: (key: string) => i18n.value(`app.toolkit.${key}.label`),
+    descriptipn: (key: string) => i18n.value(`app.toolkit.${key}.description`)
   }
   const tool = {
-    label: (key: string) => i18n.value.t(`app.tool.${key}.label`),
-    descriptipn: (key: string) => i18n.value.t(`app.tool.${key}.description`)
+    label: (key: string) => i18n.value(`app.tool.${key}.label`),
+    descriptipn: (key: string) => i18n.value(`app.tool.${key}.description`)
   }
   const tag = {
-    label: (key: string) => i18n.value.t(`app.tags.${key}.label`),
-    descriptipn: (key: string) => i18n.value.t(`app.tags.${key}.description`)
+    label: (key: string) => i18n.value(`app.tags.${key}.label`),
+    descriptipn: (key: string) => i18n.value(`app.tags.${key}.description`)
   }
 
   return {
