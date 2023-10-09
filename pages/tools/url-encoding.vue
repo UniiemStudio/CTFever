44e3a72b09b1445f0448af81a76f999f541dce7b
HoshinoSuzumi <master@uniiem.com>
2023-10-09 16:29:25 +0800

    chore: tool page placeholder

---
+++ pages/tools/url-encoding.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <ToolContainer>
+    <h1>URL Encoding Tool</h1>
+  </ToolContainer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/tools/uuid-generator.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <ToolContainer>
+    <h1>UUID Generator</h1>
+  </ToolContainer>
+</template>
+
+<style scoped>
+
+</style>
