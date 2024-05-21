aaf1f756cd0212fc488d5ed6a822b5d09ba55508
星野鈴美 <master@uniiem.com>
2024-05-21 19:24:27 +0800

    fix

--- pages/tools/zip-pseudo-encryption.vue
+++ pages/tools/zip-pseudo-encryption.vue
@@ -51,7 +51,7 @@ const onIncomeFiles = (files: File[]) => {
 
 <template>
   <ToolContainer>
-    <div class="grid grid-cols-2 md:grid-cols-1 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div class="w-full h-fit">
         <UniFileDnD
           accept="application/zip,application/x-zip-compressed"
